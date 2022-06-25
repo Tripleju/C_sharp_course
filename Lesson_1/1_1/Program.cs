20be58c3c9e06c63db4abd4d0bab2c3223a4fd76
July <filimonovajuly@gmail.com>
2022-06-25 14:54:58 +0300

    Task 1_1

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, all peaple!");
+﻿//string num_str=Console.ReadLine();
+//int num=int.Parse(num_str);
+// 
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+//  Console.WriteLine($"{num1}^2:{num1*num1}");
+if (num2 == num1 * num1)
+// Console.WriteLine(num2);
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
