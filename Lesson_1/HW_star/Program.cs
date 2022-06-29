087184c29f2d1ce719a2d962fc6a605330037c36
July <filimonovajuly@gmail.com>
2022-06-30 01:07:07 +0300

    добавила HW_star

---
+++ Lesson_1/HW_star/Program.cs
@@ -0,0 +1,14 @@
+﻿// *Напишите программу вычисления модуля числа.
+
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 0)
+
+{
+    Console.WriteLine(num);
+}
+else
+{
+    Console.WriteLine(-num);
+
+}
