945af7c330af2a54fab062acae96ded3b7752195
July <filimonovajuly@gmail.com>
2022-06-29 16:17:48 +0300

    добавила HW_2 задача 4

---
+++ Lesson_1/HW_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if(num1>num3)
+    {
+        Console.WriteLine($"max={num1}");
+    }
+    else
+    {
+        Console.WriteLine($"max={num3}");
+    }
+}
+else if(num2>num3)
+{
+    Console.WriteLine($"max={num2}");
+}
+else
+{
+    Console.WriteLine($"max={num3}");
+}
