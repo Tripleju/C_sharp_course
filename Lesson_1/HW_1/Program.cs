6c2a753b3528000b315fc09971ae436552fa0980
July <filimonovajuly@gmail.com>
2022-06-29 15:46:02 +0300

    добавила HW_1 задача 2

---
+++ Lesson_1/HW_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+
+if (num2 > num1)
+// Console.WriteLine(num2);
+{
+    Console.WriteLine($"{num2}>{num1}, max={num2}");
+}
+else
+{
+    Console.WriteLine($"{num1}>{num2}, max={num1}");
+
+}
