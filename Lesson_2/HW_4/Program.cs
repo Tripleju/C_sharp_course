38e1205deff839f35f8162dde92c0bb863dea48a
July <filimonovajuly@gmail.com>
2022-07-01 15:38:50 +0300

    добавила HW_4 задача 2

---
+++ Lesson_2/HW_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: Напишите программу, которая выводитслучайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+void SecondDelete(int num)
+{
+    Console.WriteLine($"{num / 100}" + $"{num % 10}");
+}
+
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine(num);
+SecondDelete(num);
+
