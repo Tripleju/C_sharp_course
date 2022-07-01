6396918d7969e59fbc7556e812487127be81e0b7
July <filimonovajuly@gmail.com>
2022-07-01 11:23:50 +0300

    доработала задачу 2_1

--- Lesson_2/2_1/Program.cs
+++ Lesson_2/2_1/Program.cs
@@ -1,7 +1,9 @@
-﻿int TakeNum(int num)
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает последнюю цифру этого числа.
+int TakeNum(int num)
 {
     return num % 10;
 }
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(TakeNum(num));
 
-Console.WriteLine(TakeNum(678));
-Console.WriteLine(TakeNum(678565565));
