4bb8c3d5e6712e2d000564cd5c26a8ba2b8787b6
July <filimonovajuly@gmail.com>
2022-07-07 15:55:22 +0300

    добавила примеры 1 и 2 из лекции

---
+++ Lesson_4/L_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с”.
+
+string text= "— Я думаю, — сказал князь, улыбаясь, — что,"
+            +"ежели бы вас послали вместо нашего милого"
+            +"Винценгероде, вы бы взяли приступом согласие"
+            +"прусского короля. Вы так красноречивы. Вы"
+            +"дадите мне чаю?";
+
+string Replace(string text, char OldValue, char NewValue)
+{
+    string result=String.Empty; //передаем результату исходное пустое значение
+    int length=text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i]==OldValue) result=result+$"{NewValue}";
+        else result=result+$"{text[i]}";
+    }
+    return result;
+}
+
+string newText=Replace(text,' ','|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText=Replace(newText,'к','К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText=Replace(newText,'С','с');
+Console.WriteLine(newText);
