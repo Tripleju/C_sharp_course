2aecf84ae29b4629fe626c568436a65810851e40
July <filimonovajuly@gmail.com>
2022-08-04 12:03:34 +0300

    задача 1

---
+++ Lesson_5/S2/Program.cs
@@ -0,0 +1,38 @@
+﻿// 1. Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+void Print(int[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+{
+Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
+}
+
+int[] MassNum(int size)
+{
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+arr[i] = new Random().Next(-9, 10);
+}
+return arr;
+}
+
+void ChangeNum(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+       arr[i]=-arr[i];
+    
+    }
+     Console.WriteLine();
+}
+int[] arr_1 =MassNum(12);
+Print(arr_1);
+ChangeNum(arr_1);
+Print(arr_1);
