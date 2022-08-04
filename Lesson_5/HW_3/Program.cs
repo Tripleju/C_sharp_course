8d089fb2b25ba7a6eb0d2396bd81224c76f75c79
July <filimonovajuly@gmail.com>
2022-08-04 23:44:53 +0300

    HW_3 задача 38

---
+++ Lesson_5/HW_3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным
+//  и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNum(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void DiffMaxMin(int[] arr)
+{
+    int diff = 0;
+    int max=0;
+    int min=100;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+     for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }   
+    diff=max-min;
+    Console.WriteLine($"Max Number - Min Number={diff}");
+}
+int[] arr_1 = MassNum(7);
+Print(arr_1);
+DiffMaxMin(arr_1);
