8bc175952ef11f9b9ebf8df65fd1cab60e8d6272
July <filimonovajuly@gmail.com>
2022-08-22 19:19:50 +0300

    Добавлена HW_1

---
+++ Lesson_6/HW_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+void CountPositiveNum(int size)
+{
+int[] arr = new int[size];
+int count=0;
+// Console.WriteLine("Введите число");
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите следующее число");
+        arr[i] = int.Parse(Console.ReadLine());
+        if(arr[i]>0) count++;
+    }
+Console.WriteLine($"Количество положительных чисел={count}");
+}
+
+CountPositiveNum(6);
